1c70ad656208bdb70c286a1e7b8c767289098b23
RobbiAdiansyah <robbiadiansyah@gmail.com>
2023-10-12 08:20:06 +0700

    pertama commit

---
+++ pertemuan ke 1/tugas 1 program nama.cpp
@@ -0,0 +1,9 @@
+#include "stdio.h"
+
+int main()
+{
+	printf(" nama saya : Robbi adiansyah nirwana. \n");
+	printf(" nim \t   : 2341033. \n");
+	printf(" jurursan  : teknik informatika. \n");
+	return 0;
+}
